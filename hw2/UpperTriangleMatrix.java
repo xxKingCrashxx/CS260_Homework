a295ca8f07b658e66352d0f49e31e60196b10b7b
Austin Decker <deckaj01@pfw.edu>
2022-09-14 12:00:35 -0400

    implemented clone method in UTM class

--- hw2/UpperTriangleMatrix.java
+++ hw2/UpperTriangleMatrix.java
@@ -1,6 +1,36 @@
-public class UpperTriangleMatrix 
+public class UpperTriangleMatrix implements Cloneable
 {
     private String[] elementArrary;
+
+    public UpperTriangleMatrix()
+    {
+        int n = 10;
+        elementArrary = new String[(n*(n+1))/2];
+    }
+
+    public UpperTriangleMatrix(int size)
+    {
+        elementArrary = new String[(size*(size+1))/2];
+    }
+
+    @Override
+    public UpperTriangleMatrix clone() throws CloneNotSupportedException
+    {
+        UpperTriangleMatrix clonedObj;
+        try 
+        {
+            //using object built in clone
+            clonedObj = (UpperTriangleMatrix)super.clone();
+
+            //deep copy of string array
+            clonedObj.elementArrary = elementArrary.clone();
+        } 
+        catch (CloneNotSupportedException e) 
+        {
+            throw new CloneNotSupportedException("This object does not support cloneing");
+        }
+        return clonedObj; 
+    }
 
     public static UpperTriangleMatrix concat(UpperTriangleMatrix utm1, UpperTriangleMatrix utm2)
     {
