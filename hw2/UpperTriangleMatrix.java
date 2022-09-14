cec3bf2db5ed7eca4301ad4898ba3f3bf89b77be
xxKingCrashxx <ajdecker2@gmail.com>
2022-09-14 10:05:36 -0400

    init commit

---
+++ hw2/UpperTriangleMatrix.java
@@ -0,0 +1,35 @@
+public class UpperTriangleMatrix 
+{
+    private String[] elementArrary;
+
+    public static UpperTriangleMatrix concat(UpperTriangleMatrix utm1, UpperTriangleMatrix utm2)
+    {
+        return null;
+    }
+
+    public static UpperTriangleMatrix largestCombo(UpperTriangleMatrix utm1, UpperTriangleMatrix utm2)
+    {
+        return null;
+    }
+
+    public boolean equals(Object ob)
+    {
+        return false;
+    }
+
+    public void setElement(int row, int col, String data)
+    {
+
+    }
+
+    public String getElement(int row, int col)
+    {
+        return null;
+    }
+
+    public String toString()
+    {
+        return null;
+    }
+    
+}
