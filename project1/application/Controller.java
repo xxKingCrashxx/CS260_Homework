86e0f382085438681583b954e82dadfca4020f03
Austin Decker <deckaj01@pfw.edu>
2022-10-05 17:44:34 -0400

    modified mainLayout & Controller.java

--- project1/application/Controller.java
+++ project1/application/Controller.java
@@ -1,5 +1,34 @@
 package application;
 
-public class Controller {
+import javafx.fxml.FXML;
+import javafx.scene.control.DatePicker;
+import javafx.scene.control.ListView;
+import javafx.scene.control.TextField;
+import javafx.scene.input.MouseEvent;
+import structures.CarDataNode;
+import structures.GarageExitBag;
+import structures.GarageSet;
+
+public class Controller 
+{
+    @FXML
+    private TextField txtRemoveCar;
+    @FXML
+    private TextField txtAddCar;
+    @FXML
+    private DatePicker datePicker;
+    @FXML
+    private ListView<CarDataNode> lstParkedCarNodes;
+    @FXML
+    private ListView<CarDataNode> lstRemovedCarNodes;
+
+    private GarageSet parkedCars = new GarageSet();
+    private GarageExitBag removedCars = new GarageExitBag();
+
+
+    public void exitProgram(MouseEvent mouseEvent)
+    {
+        System.exit(0);
+    }
     
 }
