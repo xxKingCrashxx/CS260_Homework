3141ddfb2eb3a783f34ad22a880dd747ec55660f
xxKingCrashxx <ajdecker2@gmail.com>
2022-10-06 12:10:40 -0400

    added more method implementations

--- project1/structures/GarageSet.java
+++ project1/structures/GarageSet.java
@@ -1,10 +1,19 @@
 package structures;
+
+
+
 public class GarageSet
 {
     private CarDataNode head;
     private CarDataNode tail;
     private int totalItems;
 
+    public GarageSet()
+    {
+        head = null;
+        tail = null;
+        totalItems = 0;
+    }
 
     public void checkIn(int index)
     {
@@ -12,6 +21,27 @@ public class GarageSet
     }
     public void checkIn(String liscenceID)
     {
+        CarDataNode newNode = new CarDataNode(liscenceID, null, null);
+        if(this.head == null)
+        {
+            head = newNode;
+            tail = newNode;
+        }
+        else
+        {
+            CarDataNode searchNode = head;
+
+            if(!hasDuplicate(newNode))
+            {
+                while(searchNode.getNext() != null){
+                    searchNode = searchNode.getNext();
+                }
+                searchNode.addNodeAfter(newNode);
+                tail = newNode;
+            }
+
+        }
+        totalItems++;
 
     }
 
@@ -22,6 +52,19 @@ public class GarageSet
     public void checkOut(String liscenceID)
     {
 
+    }
+
+    private boolean hasDuplicate(CarDataNode node)
+    {
+        CarDataNode cursor = head;
+
+        for (; cursor != null; cursor = cursor.getNext()) 
+        {
+            if(cursor.getCarID().equals(node.getCarID())){
+                return true;
+            }
+        }
+        return false;
     }
 
     @Override
@@ -39,5 +82,7 @@ public class GarageSet
     {
         
     }
+
+    
 
 }
