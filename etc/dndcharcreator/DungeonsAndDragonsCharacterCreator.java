0ddf5873d6fb3b2be33b70a192d26f5f78b6b057
Austin Decker <ajdecker2@gmail.com>
2022-10-11 20:50:25 -0400

    removed etc files

--- etc/dndcharcreator/DungeonsAndDragonsCharacterCreator.java
+++
@@ -1,9 +0,0 @@
-public class DungeonsAndDragonsCharacterCreator
-{
-	public static void main(String[] args){
-		Item item = new Item("sword", 1, 2.5);
-		item.calculateItemWeight();
-		item.toString();
-
-	}
-}
