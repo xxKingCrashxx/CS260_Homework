3ba2d3b35d836c93d649537d0c0ba0137d8a8243
xxking_crashxx <ajdecker2@gmail.com>
2022-11-11 11:56:42 -0500

    reformatted class, made variables private.

--- AustinDecker_Project3/AustinDecker_Project3/src/VideoGame.java
+++ AustinDecker_Project3/AustinDecker_Project3/src/VideoGame.java
@@ -1,28 +1,20 @@
 import java.io.Serializable;
 
 public class VideoGame implements Serializable, Comparable<VideoGame>{
-    int price;
-    String name;
+    private int price;
+    private String name;
 
     public VideoGame(String name, int price){
         //TODO
     }
 
-    public int getPrice() {
-        return price;
-    }
+    public int getPrice() {return price;}
 
-    public String getName() {
-        return name;
-    }
+    public String getName() {return name;}
 
-    public void setPrice(int price) {
-        this.price = price;
-    }
+    public void setPrice(int price) {this.price = price;}
 
-    public void setName(String name) {
-        this.name = name;
-    }
+    public void setName(String name) {this.name = name;}
     
     @Override
     public int compareTo(VideoGame o) {
