fe495a9de3226347150db976c7d0a53f7dc4cab6
xxKingCrashxx <ajdecker2@gmail.com>
2022-11-21 15:17:41 -0500

    started implementation of load/save from file.

--- AustinDecker_Project3/AustinDecker_Project3/src/DataBaseAccess.java
+++ AustinDecker_Project3/AustinDecker_Project3/src/DataBaseAccess.java
@@ -1,3 +1,10 @@
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+
 import structures.AVLTree;
 import structures.Node;
 
@@ -30,11 +37,45 @@ public class DataBaseAccess {
         return foundNode == null ? null : foundNode.getData();
     }
 
-    public static void loadDataBaseFromFile(){
-        //TODO
+    public static void loadDataBaseFromFile(DataBaseAccess dataBaseAccess){
+        FileInputStream fInputStream;
+        ObjectInputStream objectInputStream;
+
+        try {
+            fInputStream = new FileInputStream("db_save.txt");
+            objectInputStream = new ObjectInputStream(fInputStream);
+
+            dataBaseAccess = (DataBaseAccess)objectInputStream.readObject();
+
+        } catch (FileNotFoundException fnfe) {
+            // TODO: handle exception
+        } catch (IOException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        } catch (ClassNotFoundException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
     }
 
-    public static void saveDataBaseToFile(){
+    public static void saveDataBaseToFile(DataBaseAccess dataBaseAcess){
         //TODO
+        FileOutputStream fOutputStream;
+        ObjectOutputStream objectOutputStream;
+        try {
+            fOutputStream = new FileOutputStream("db_save.txt");
+            objectOutputStream = new ObjectOutputStream(fOutputStream);
+            objectOutputStream.writeObject(dataBaseAcess.database);
+
+            objectOutputStream.close();
+
+
+        } catch (FileNotFoundException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        } catch (IOException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
     }
 }
