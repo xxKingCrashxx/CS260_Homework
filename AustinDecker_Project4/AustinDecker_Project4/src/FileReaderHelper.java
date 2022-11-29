7e6812fd5569909a0729fc83973c9ef79fdcc786
xxking_crashxx <ajdecker2@gmail.com>
2022-11-29 12:51:42 -0500

    added documentation for FileReaderHelper.java

--- AustinDecker_Project4/AustinDecker_Project4/src/FileReaderHelper.java
+++ AustinDecker_Project4/AustinDecker_Project4/src/FileReaderHelper.java
@@ -2,8 +2,21 @@ import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.ArrayList;
 import java.util.Scanner;
-
+/**
+ * Helper class for reading lines of data from a specified text file
+ * @author Austin Decker
+ * @version 1.0
+ * 
+ */
 public class FileReaderHelper {
+    /**
+     * method which reads a specified file and saves each line into an arraylist for later access.
+     * Method returns true if the reading was successful and false if file was not found or reading was unsuccessful.
+     * @throws NullPointerException
+     * @param filepath String
+     * @param lines ArrayList<String>
+     * @return boolean
+     */
     public static boolean readFile(String filepath, ArrayList<String> lines){
 
         if(lines == null)
