72d6c660e3c0246c5e3c8250806c88479467336b
xxking_crashxx <ajdecker2@gmail.com>
2022-11-29 13:03:53 -0500

    added field variables, modified constructor.

--- AustinDecker_Project4/AustinDecker_Project4/src/HeapTree.java
+++ AustinDecker_Project4/AustinDecker_Project4/src/HeapTree.java
@@ -1,7 +1,15 @@
 public class HeapTree<T extends Comparable<T>> {
+    private final int INIT_SIZE;
+    private int currentIndex = 0;
     private T[] objects;
 
+    public HeapTree(){
+        INIT_SIZE = 10;
+        objects = (T[])new Object[INIT_SIZE];
+    }
+    
     public HeapTree(int treeSize){
+        INIT_SIZE = treeSize;
         objects = (T[])new Object[treeSize];
     }
 } 
