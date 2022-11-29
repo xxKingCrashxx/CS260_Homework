81ca5a57d02ec76cc6836531b56503d32613cdf5
xxking_crashxx <ajdecker2@gmail.com>
2022-11-29 12:24:29 -0500

    started on main program implementation.

--- AustinDecker_Project4/AustinDecker_Project4/src/App.java
+++ AustinDecker_Project4/AustinDecker_Project4/src/App.java
@@ -1,5 +1,66 @@
+import java.util.Scanner;
+
 public class App {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        boolean running = true;
+        do{
+            System.out.println("==================================================================");
+            System.out.println("Options:\n"
+                    + "\t0:\tExit\n"
+                    + "\t1:\tBuild Heap\n"
+                    + "\t2:\tInsert\n"
+                    + "\t3:\tDelete\n"
+                    + "\t4:\tHeap sort\n"
+                    + "\t5:\tPrint\n");
+            System.out.println("==================================================================");
+
+            String usrInput = scanner.nextLine();
+            switch (usrInput){
+                case "0":
+                    running = false;
+                    break;
+                case "1":
+                    buildHeap();
+                    break;
+                case "2":
+                    insert();
+                    break;
+                case "3":
+                    delete();
+                    break;
+                case "4":
+                    heapSort();
+                    break;
+                case "5":
+                    print();
+                    break;
+                default:
+                    System.out.println("Not a valid input.");
+            }
+        } while(running);
+
+        scanner.close();
+        System.out.println("closing program.");
+    }
+
+    private static void print() {
+
+    }
+
+    private static void heapSort() {
+
+    }
+
+    private static void delete() {
+
+    }
+
+    private static void insert() {
+
+    }
+
+    private static void buildHeap() {
+
     }
 }
